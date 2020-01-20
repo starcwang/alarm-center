5d73faf96e894f3e2cdf02dd75a552527a995567
wangchao <linchao.wc@alibaba-inc.com>
2020-01-20 17:53:51 +0800

    init

--- alarm-center-web/src/main/java/com/star/alarmcenter/web/AlarmCenterWebApplication.java
+++ alarm-center-web/src/main/java/com/star/alarmcenter/web/AlarmCenterWebApplication.java
@@ -1,13 +1,19 @@
-package com.star.alarmcenter.api;
+package com.star.alarmcenter.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 报警中心的Web应用程序
+ *
+ * @author wangchao
+ * @date 2020/01/20
+ */
 @SpringBootApplication
-public class AlarmCenterApiApplication {
+public class AlarmCenterWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlarmCenterApiApplication.class, args);
+        SpringApplication.run(AlarmCenterWebApplication.class, args);
     }
 
 }
