deccda23d62b76057882323520b2cfac6b9f9431
wangchao <linchao.wc@alibaba-inc.com>
2020-01-20 11:06:48 +0800

    Initial commit

---
+++ alarm-center-model/src/main/java/com/star/alarmcenter/model/A.java
@@ -0,0 +1,16 @@
+package com.star.alarmcenter.model;
+
+/**
+ * @author wangchao
+ * @date 2020/01/19
+ */
+public class A {
+
+    /**
+     *
+     */
+    public static class BusTest {
+
+    }
+
+}
---
+++ alarm-center-web/src/main/java/com/star/alarmcenter/web/AlarmCenterWebApplication.java
@@ -0,0 +1,13 @@
+package com.star.alarmcenter.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmCenterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmCenterApiApplication.class, args);
+    }
+
+}
