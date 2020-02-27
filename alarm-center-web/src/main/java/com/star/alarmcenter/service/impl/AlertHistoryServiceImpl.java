5af3d6e2fd641a709e07214baf7dc2023c2d61f5
wangchao <linchao.wc@alibaba-inc.com>
2020-02-27 20:24:38 +0800

    :construction: init

--- alarm-center-web/src/main/java/com/star/alarmcenter/service/impl/AlertHistoryServiceImpl.java
+++ alarm-center-web/src/main/java/com/star/alarmcenter/service/impl/AlertHistoryServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AlertHistoryServiceImpl implements AlertHistoryService {
 
+
     @Resource
     private AlertHistoryDAO alertHistoryDAO;
 
